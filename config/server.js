70ee5bdae1a404542f197198c3c620e0a919ceb6
zzhuravleff <aqua197604@gmail.com>
2024-03-20 00:30:37 +0300

    0.0.2

--- config/server.js
+++ config/server.js
@@ -1,6 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', 'studio.zzhur.ru'),
   app: {
     keys: env.array('APP_KEYS'),
   },
